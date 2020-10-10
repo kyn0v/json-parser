b1ba9934106e21fab3ac0976728b8a5f24fd85e8
kyn0v <2275169040@qq.com>
2020-10-10 08:57:17 +0800

    feat: singlar单元测试 + 解析true/false

--- leptjson.h
+++ leptjson.h
@@ -1,17 +1,25 @@
 #ifndef LEPTJSON_H__
 #define LEPTJSON_H__
 
-enum lept_type { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT };
-
-struct lept_value{
-    lept_type type;
+enum lept_type {
+	LEPT_NULL,
+	LEPT_FALSE,
+	LEPT_TRUE,
+	LEPT_NUMBER,
+	LEPT_STRING,
+	LEPT_ARRAY,
+	LEPT_OBJECT
 };
 
-enum status {
-    LEPT_PARSE_OK = 0,
-    LEPT_PARSE_EXPECT_VALUE,
-    LEPT_PARSE_INVALID_VALUE,
-    LEPT_PARSE_ROOT_NOT_SINGULAR
+struct lept_value {
+	lept_type type;
+};
+
+enum lept_status {
+	LEPT_PARSE_OK = 0,
+	LEPT_PARSE_EXPECT_VALUE,
+	LEPT_PARSE_INVALID_VALUE,
+	LEPT_PARSE_ROOT_NOT_SINGULAR
 };
 
 int lept_parse(lept_value* v, const char* json);
