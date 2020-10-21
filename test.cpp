5f8e42e530f445a41c9e10f1e2609e3d13499e65
kyn0v <2275169040@qq.com>
2020-10-21 23:52:52 +0800

    test: 添加数值过大测试用例

--- test.cpp
+++ test.cpp
@@ -124,6 +124,11 @@ static void test_parse_number() {
 	TEST_NUMBER(-1.7976931348623157e+308, "-1.7976931348623157e+308");
 }
 
+static void test_parse_number_too_big() {
+	TEST_ERROR(LEPT_PARSE_NUMBER_TOO_BIG, "1e309");
+	TEST_ERROR(LEPT_PARSE_NUMBER_TOO_BIG, "-1e309");
+}
+
 static void test_parse() {
 	test_parse_null();
 	test_parse_true();
@@ -132,6 +137,7 @@ static void test_parse() {
 	test_parse_invalid_value();
 	test_parse_root_not_singular();
 	test_parse_number();
+	test_parse_number_too_big();
 }
 
 int main() {
