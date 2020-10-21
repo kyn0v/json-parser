8187c00f608c66159caa05b366f42c6e5b650599
kyn0v <2275169040@qq.com>
2020-10-21 23:49:47 +0800

    bug: 解析结果为NOT_SINGULAR时应将type置为null

--- leptjson.cpp
+++ leptjson.cpp
@@ -98,6 +98,7 @@ int lept_parse(lept_value* v, const char* json) {
 	if (ret == LEPT_PARSE_OK) {
 		lept_parse_whitespace(&c);
 		if (*c.json != '\0') {
+			v->type = LEPT_NULL;
 			ret = LEPT_PARSE_ROOT_NOT_SINGULAR;
 		}
 	}
